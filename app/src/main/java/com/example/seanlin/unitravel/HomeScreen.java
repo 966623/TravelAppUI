ddf9922a2497daab8f393c1f677283a9acd69e36
Sean Lin <sean.j.lin@gmail.com>
2016-04-18 16:47:33 -0500

    Add screens, theme,

--- app/src/main/java/com/example/seanlin/unitravel/HomeScreen.java
+++ app/src/main/java/com/example/seanlin/unitravel/HomeScreen.java
@@ -2,12 +2,26 @@ package com.example.seanlin.unitravel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 public class HomeScreen extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
+
+    public void onButtonClick(View view){
+        //do something when button is clicked.
+        Intent i = new Intent(getApplicationContext(), NewTripScreen.class);
+        startActivity(i);
     }
 }
