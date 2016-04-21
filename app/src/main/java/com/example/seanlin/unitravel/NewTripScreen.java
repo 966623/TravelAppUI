1dda2811f0c366b66b8e4adeea40da5391552ea8
Sean Lin <sean.j.lin@gmail.com>
2016-04-20 23:51:11 -0500

    Syntax error fixes, add new trip to summary page transition

--- app/src/main/java/com/example/seanlin/unitravel/NewTripScreen.java
+++ app/src/main/java/com/example/seanlin/unitravel/NewTripScreen.java
@@ -1,7 +1,9 @@
 package com.example.seanlin.unitravel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewTripScreen extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class NewTripScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_trip_screen);
+    }
+    public void onNewTripButtonClick(View view){
+        //do something when button is clicked.
+        Intent i = new Intent(getApplicationContext(), SummaryPage.class);
+        startActivity(i);
     }
 }
